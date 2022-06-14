066b02f778771d1239a2e9b3cb6df93da4bc7a0f
RamanaiahPirla <ramanaiah.237@gmail.com>
2022-06-14 21:25:05 +0530

    updated code for few more APIs

--- coinswitch/src/main/java/org/coinswitch/controller/ExploreCryptoController.java
+++ coinswitch/src/main/java/org/coinswitch/controller/ExploreCryptoController.java
@@ -1,6 +1,7 @@
 package org.coinswitch.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.coinswitch.entities.CryptoCurrency;
 import org.coinswitch.repository.CryptoRepository;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,25 +22,39 @@ import io.swagger.annotations.ApiParam;
 @RequestMapping("/crypto")
 public class ExploreCryptoController {
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(ExploreCryptoController.class);
-	
+
 	@Autowired
 	private CryptoRepository cryptoRepo;
 
 	@PostMapping("/createCrypto")
 	public ResponseEntity<CryptoCurrency> createCrypto(
 			@ApiParam(value = "Crypto Details", required = true) @RequestBody CryptoCurrency crypto) {
-		logger.info(" $$$$$$$$$$$$$$$$$$$$$$         Entered into createCrypto $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ");
-		
+		logger.info(" $$$$$$$$$$$$$$$$$$$$$$         Entered into createCrypto     $$$$$$$$$$$$$$$$$$$$$$$$$$$$ ");
+
 		return new ResponseEntity<CryptoCurrency>(cryptoRepo.save(crypto), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/getAllCryptoCurrency")
-	public ResponseEntity<List<CryptoCurrency>> fetchAllCryptoCurrency(){
-		return new ResponseEntity<List<CryptoCurrency>>(cryptoRepo.findAll(),HttpStatus.OK);
+	public ResponseEntity<List<CryptoCurrency>> fetchAllCryptoCurrency() {
+		return new ResponseEntity<List<CryptoCurrency>>(cryptoRepo.findAll(), HttpStatus.OK);
+	}
+
+	@GetMapping("/findByCurrencyId/{currencyId}")
+	public ResponseEntity<CryptoCurrency> getCurrencyDetailsById(
+			@ApiParam(value = "Currency Id", required = true) @PathVariable("currencyId") Integer currencyId) {
+		Optional<CryptoCurrency> crypto = cryptoRepo.findById(currencyId);
+		if(crypto.isPresent()) {
+			return new ResponseEntity<CryptoCurrency>(crypto.get(), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 	
-	
-	
-	
-	
+	@GetMapping("/getCurrencyByKeyword")
+	public ResponseEntity<List<CryptoCurrency>> getCurrencyBycryptoSymbol(@ApiParam(value = "Currency By CryptoSymbol", required = true) @PathVariable("crypto_symbol") String  crypto_symbol){
+		
+		List<CryptoCurrency> currency = cryptoRepo.findBycryptoSymbol(crypto_symbol);
+		
+		return new ResponseEntity<List<CryptoCurrency>>(currency, HttpStatus.OK);
+		
+	}
 }
--- coinswitch/src/main/java/org/coinswitch/repository/CryptoRepository.java
+++ coinswitch/src/main/java/org/coinswitch/repository/CryptoRepository.java
@@ -1,11 +1,17 @@
 package org.coinswitch.repository;
 
+import java.util.List;
+
 import org.coinswitch.entities.CryptoCurrency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CryptoRepository extends JpaRepository<CryptoCurrency, Integer> {
+
+	@Query("SELECT crypto_symbol FROM crypto_currency cc WHERE cc.crypto_symbol LIKE %:crypto_symbol%")
+	public List<CryptoCurrency> findBycryptoSymbol(String crypto_symbol);
 	
 
 }
